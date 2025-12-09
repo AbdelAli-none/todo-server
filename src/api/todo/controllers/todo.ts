095a14c3b03a8678b70db6a7235a2c079176c577
Ali <topolovskoda@gmail.com>
2025-12-09 07:19:27 +0100

    server side files

---
+++ src/api/todo/controllers/todo.ts
@@ -0,0 +1,11 @@
+/**
+ * todo controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController("api::todo.todo");
+
+// src / api / todo / controllers / todo.ts;
+
+// src / api / todo / controllers / todo.ts;
